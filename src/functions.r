ff02729de58a776ce1f7518a995f590eff2bd8a1
Tanoh Anne-Laure <anne-laure.tanoh@curie.fr>
2026-06-25 16:12:43 +0200

    Cleaning code: putting all functions in functions.r script

--- src/functions.r
+++ src/functions.r
@@ -68,3 +68,129 @@ auc_manual <- function(response, predictor) {
   auc <- (sum(r[df$response == 1]) - n1 * (n1 + 1) / 2) / (n1 * n0)
   return(auc)
 }
+
+# -----------------------------
+# Helper functions to compute summaries
+# -----------------------------
+
+get_monthly_slopes <- function(time_grid, y_ctrl, y_trt, months = 1:6) {
+
+  sapply(months, function(m) {
+
+    target_time <- m * 30
+    idx <- which.min(abs(time_grid - target_time))
+
+    if (idx >= length(time_grid)) return(NA)
+
+    dy_c <- (y_ctrl$pred[idx + 1] - y_ctrl$pred[idx]) /
+            (time_grid[idx + 1] - time_grid[idx])
+
+    dy_t <- (y_trt$pred[idx + 1] - y_trt$pred[idx]) /
+            (time_grid[idx + 1] - time_grid[idx])
+
+    dy_t - dy_c
+  })
+}
+
+
+compute_auc <- function(y, time_grid) {
+  sum(diff(time_grid) * (head(y$pred[,1], -1) + tail(y$pred[,1], -1)) / 2)
+}
+get_weekly_nadir <- function(time_grid, pred, week_size = 7) {
+
+  week_id <- floor(time_grid / week_size)
+  aggregate(pred[,1],
+             by = list(week = week_id),
+             FUN = min,
+             na.rm = TRUE)
+  }
+
+get_monthly_pct_reduction <- function(time_grid, y_ctrl, y_trt, months = 1:6){
+
+  baseline_ctrl <- y_ctrl$pred[1,1]
+  baseline_trt  <- y_trt$pred[1,1]
+
+  sapply(months, function(m){
+
+    idx <- which.min(abs(time_grid - m*30))
+
+    pct_ctrl <-
+      (baseline_ctrl - y_ctrl$pred[idx,1]) /
+      baseline_ctrl
+
+    pct_trt <-
+      (baseline_trt - y_trt$pred[idx,1]) /
+      baseline_trt
+
+    pct_trt - pct_ctrl
+  })
+}
+
+# -----------------------------
+# Helper functions for bootstrap
+# -----------------------------
+get_r2_models <- function(data) {
+
+  models <- list(
+    nadir = lm(logHR ~ delta_nadir, data = data, weights = 1/(SE_logHR^2)),
+    time_nadir = lm(logHR ~ delta_time_nadir, data = data, weights = 1/(SE_logHR^2)),
+    auc = lm(logHR ~ delta_auc, data = data, weights = 1/(SE_logHR^2)),
+    pct1 = lm(logHR ~ pct_m1, data = data, weights = 1/(SE_logHR^2)),
+    pct2 = lm(logHR ~ pct_m2, data = data, weights = 1/(SE_logHR^2)),
+    pct3 = lm(logHR ~ pct_m3, data = data, weights = 1/(SE_logHR^2)),
+    pct4 = lm(logHR ~ pct_m4, data = data, weights = 1/(SE_logHR^2)),
+    pct5 = lm(logHR ~ pct_m5, data = data, weights = 1/(SE_logHR^2)),
+    pct6 = lm(logHR ~ pct_m6, data = data, weights = 1/(SE_logHR^2)),
+    slope1 = lm(logHR ~ slope_m1, data = data, weights = 1/(SE_logHR^2)),
+    slope2 = lm(logHR ~ slope_m2, data = data, weights = 1/(SE_logHR^2)),
+    slope3 = lm(logHR ~ slope_m3, data = data, weights = 1/(SE_logHR^2)),
+    slope4 = lm(logHR ~ slope_m4, data = data, weights = 1/(SE_logHR^2)),
+    slope5 = lm(logHR ~ slope_m5, data = data, weights = 1/(SE_logHR^2)),
+    slope6 = lm(logHR ~ slope_m6, data = data, weights = 1/(SE_logHR^2)),
+    abs1 = lm(logHR ~ absolutediff_m1, data = data, weights = 1/(SE_logHR^2)),
+    abs2 = lm(logHR ~ absolutediff_m2, data = data, weights = 1/(SE_logHR^2)),
+    abs3 = lm(logHR ~ absolutediff_m3, data = data, weights = 1/(SE_logHR^2)),
+    abs4 = lm(logHR ~ absolutediff_m4, data = data, weights = 1/(SE_logHR^2)),
+    abs5 = lm(logHR ~ absolutediff_m5, data = data, weights = 1/(SE_logHR^2)),
+    abs6 = lm(logHR ~ absolutediff_m6, data = data, weights = 1/(SE_logHR^2))
+  )
+
+  sapply(models, function(m) summary(m)$r.squared)
+}
+# -----------------------------
+# Helper functions to plot LOSO
+# -----------------------------
+
+plot_loso <- function(summary_name, loso_df) {
+
+  df <- loso_df %>%
+    dplyr::filter(summary == summary_name) %>%
+    dplyr::arrange(R2)
+
+  ggplot(
+    df,
+    aes(
+      x = reorder(left_out_trial, R2),
+      y = R2
+    )
+  ) +
+    geom_point(size = 3) +
+    geom_segment(
+      aes(
+        x = left_out_trial,
+        xend = left_out_trial,
+        y = 0,
+        yend = R2
+      )
+    ) +
+    coord_flip() +
+    labs(
+      title = paste0(
+        "Leave-one-study-out analysis: ",
+        summary_name
+      ),
+      x = "Trial omitted",
+      y = expression(R^2)
+    ) +
+    theme_bw()
+}
