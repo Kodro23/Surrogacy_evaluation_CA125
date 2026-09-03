d75f093e5a6910e02b0fc81441c6bb88d8ba7e27
Tanoh Anne-Laure <anne-laure.tanoh@curie.fr>
2026-09-03 17:05:50 +0200

    Surrogacy eval with meta analytic model on kelim subset

--- src/install_librairies.r
+++ src/install_librairies.r
@@ -7,7 +7,6 @@ install.packages(
   ),
   dependencies = TRUE
 )
-pak::pak("DenisRustand/INLAjoint")
 install_packages<-function(path_to_requirement){
     packages <- readLines(file.path(path_to_requirement, "requirements.txt"))
     packages <- packages[!grepl("^#", packages)]
